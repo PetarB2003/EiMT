afc195df5392fd6ad1d5f0b380cf7bbdf541b628
PetarB2003 <bogatinoskipetar2003@gmail.com>
2026-03-19 16:56:57 +0100

    Create main application class EiMTApplication.java

---
+++ src/main/java/com/example/eimt/EiMTApplication.java
@@ -0,0 +1,15 @@
+package com.example.eimt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class EiMTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EiMTApplication.class, args);
+    }
+
+}
